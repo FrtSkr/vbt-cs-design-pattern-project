8b9795f3d2045b8e837c2ad6504141a26ef5d40c
Fırat Şakar <frtskr0@gmail.com>
2022-07-08 21:37:25 +0300

    done design pattern

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static VbtStaj2022DesignPattern.Interpret;
+
+namespace VbtStaj2022DesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Description1 = "Futbol maclarini çok sevsem de, genelde izlemiyorum";
+            string Description2 = "Bebegimin sagligi için anne sütüne çok önem veriyorum.";
+            string Description3 = "Spor'a giderken arabayı güzellikle vurdum.";
+
+            Console.WriteLine($"{Description1} (Erkek):");
+            Console.WriteLine(InterpretPattern.getMaleExpression().Interpret(Description1));
+
+            bool expResult = false;
+            Console.WriteLine($"{Description2} (Kadın:)");
+            foreach (Expression exp in InterpretPattern.getFemaleExpression())
+            {
+                if (exp.Interpret(Description2)) { expResult = true; break; }
+            }
+            Console.WriteLine(expResult);
+
+            bool expResult2 = false;
+            Console.WriteLine($"{Description3} (Kadın:)");
+            foreach (Expression exp in InterpretPattern.getFemaleExpression())
+            {
+                if (exp.Interpret(Description3)) { expResult2 = true; break; }
+            }
+            Console.WriteLine(expResult2);
+        }
+    }
+}
